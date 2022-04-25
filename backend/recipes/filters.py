4c4e96cc20871c60c3504538b81f586b0ba0d190
JonMakko <danila-bibik2017@yandex.ru>
2022-04-25 21:28:26 +0700

    second complete

--- backend/recipes/filters.py
+++ backend/recipes/filters.py
@@ -33,5 +33,5 @@ class RecipeFilter(filters.FilterSet):
     def get_is_in_shopping_cart(self, queryset, name, value):
         user = self.request.user
         if value:
-            return Recipe.objects.filter(customers__user=user)
+            return Recipe.objects.filter(purchase__user=user)
         return Recipe.objects.all()
